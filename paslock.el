4035113d614ac207e5acfcbe6029babb7df4411c
Guillaume Papin <guillaume.papin@epitech.eu>
2011-10-08 17:10:11 +0200

    Gestion du 'random' plus mieux.

--- paslock.el
+++ paslock.el
@@ -1,17 +1,21 @@
 ;; Auto generated code, do not edit manually !
 
-(let ((magic (random 5)))
+(let ((magic (cadr (current-time))))
   (cond
-   ((eq magic 1)
+   ((or (oddp (- magic 1))
+        (oddp (- magic 5)))
     (message "c-stuff")
     (require 'c-stuff nil t))
-   ((eq magic 2)
+   ((or (oddp (- magic 2))
+        (oddp (- magic 6)))
     (message "rick-roll")
     (require 'rick-roll nil t))
-   ((eq magic 3)
+   ((or (oddp (- magic 3))
+        (oddp (- magic 7)))
     (message "satanized-keyboard")
     (require 'satanized-keyboard nil t))
-   ((eq magic 4)
+   ((or (oddp (- magic 4))
+        (oddp (- magic 8)))
     (message "annoying-splitting")
     (require 'annoying-splitting nil t))
    (t
