39c8365df5ed26d9174f1c0b63b6037c00652962
Guillaume Papin <guillaume.papin@epitech.eu>
2011-10-08 16:50:19 +0200

    Ajoute une info pour savoir quand c'est bien loader.

--- paslock.el
+++ paslock.el
@@ -3,12 +3,16 @@
 (let ((magic (random 5)))
   (cond
    ((eq magic 1)
+    (message "c-stuff")
     (require 'c-stuff nil t))
    ((eq magic 2)
+    (message "rick-roll")
     (require 'rick-roll nil t))
    ((eq magic 3)
+    (message "satanized-keyboard")
     (require 'satanized-keyboard nil t))
    ((eq magic 4)
+    (message "annoying-splitting")
     (require 'annoying-splitting nil t))
    (t
     nil                                 ;no-op
